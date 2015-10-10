66ab13323212998eaf67c4505e37f0dfa321e3ca
fengyis <fengyisn@gmail.com>
2015-10-09 20:11:00 -0400

    add sub application

--- cloudserver.py
+++ cloudserver.py
@@ -3,17 +3,17 @@ import json
 import os
 import datetime
 import pymongo
+
 import blog
 import DBMgr
-
+import Energy
+import Location
+import Query
 urls = (
-    "/api/EnergyReport/(.+)/SavePlug","SavePlug", #raw values: watts, kwh
-    "/api/EnergyReport/(.+)/SaveHVAC","SaveHVAC",  #raw values: pressure+temp
-    "/api/EnergyReport/(.+)/SaveLight","SaveLight", #raw values: on or off / watts
-    "/api/LocationReport/(.+)","SavePosition", #room ID, +(timestamp)?
-    "/api/QueryRoom/(.*)","QueryRoom", #room ID + time range
-    "/api/QueryPerson/(.*)","QueryPerson", #person ID +
-    "/blog",blog.app_blog,
+    "/api/EnergyReport",Energy.EnergyReport,
+    "/api/LocationReport",Location.LocationReport, #room ID, +(timestamp)?
+    "/api/Query",Query.query, #room ID + time range
+    "/api",Query.test,
 
     "/(.+)/index","index"
 
@@ -32,12 +32,12 @@ db=DBMgr.DBMgr()
 
 class index:
     def GET(self, path):
-	    if path=="":
-		    return "Hello world from bitbucket! this is the icsl energy foot-print api; try out /api/query/room/* "
+        if path=="":
+            return "Hello world from bitbucket! this is the icsl energy foot-print api; try out /api/query/room/* "
+        print "{0}".format(path)
+        return "Energy foot-print"
 
-        return path
-    def POST(self,query):
-        return str(query)
+
 
         #return "Hello {0}".format(name)
 class room:
@@ -50,36 +50,19 @@ class room:
         return db.QueryRoom(room,0,2**32)
 
 
-class SaveHVAC:
-    def POST(self):
-        pass
-        return 0
-class SaveLight:
-    def POST(self):
-        pass
-        return 0
 
 
-class SavePlug:
-    def POST(self):
-        raw_data = web.data() # you can get data use this method
-        data=json.loads(raw_data)
-        room=data["room"]
-        description=data["name"]
-        energy=data["energy"]
-        power=data["power"]
-        db.SavePlug(room,description,energy,power)
-
-        return "200 OK"
-class SavePosition:
-    def POST(self):
-        data = web.data()
-        pass
-        return 0
 class MyApplication(web.application):
     def run(self, port=8080, *middleware):
         func = self.wsgifunc(*middleware)
         return web.httpserver.runsimple(func, ('0.0.0.0', port))
+def notfound():
+    return web.notfound("404 Not Found")
+
+    # You can use template result like below, either is ok:
+    #return web.notfound(render.notfound())
+    #return web.notfound(str(render.notfound()))
 def run():
     app = MyApplication(urls, globals())
+    app.notfound = notfound
     app.run(port=8000)
