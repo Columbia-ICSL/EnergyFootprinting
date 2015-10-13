212662686ca2331e96f45f5855c181c6546f05f0
Chen Xiaoqi <chenxq13@mails.tsinghua.edu.cn>
2015-10-13 12:42:01 -0400

    handle POST queries

--- Query.py
+++ Query.py
@@ -15,8 +15,12 @@ class QueryRoom:
     def GET(self,position):
         return "position is: " +position
 class QueryPerson:
+    def all(self,persion):
+        "person name: {0}".format(person)
+    def POST(self,person):
+        return self.all(person)
     def GET(self,person):
-        return "person name: {0}".format(person)
+        return self.all(person)
 #
 #
 query = web.application(urls, locals())
