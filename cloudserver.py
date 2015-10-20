cb91cd4effa14eed80f0c2469fa1fee245dec113
fengyis <fengyisn@gmail.com>
2015-10-20 13:08:45 -0400

    add chart

--- cloudserver.py
+++ cloudserver.py
@@ -13,8 +13,7 @@ urls = (
     "/api/EnergyReport",Energy.EnergyReport,
     "/api/LocationReport",Location.LocationReport, #room ID, +(timestamp)?
     "/api/Query",Query.query, #room ID + time range
-    "/frontend/(.+)", frontend,
-
+    "/frontend/(.+)", "frontend",
     "/(.+)/index","index"
 
 )
