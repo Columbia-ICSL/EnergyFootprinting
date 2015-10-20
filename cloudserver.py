48c365f8b99e5b2406948cae498fce896a36d7b5
fengyis <fengyisn@gmail.com>
2015-10-20 13:10:15 -0400

    add chart

--- cloudserver.py
+++ cloudserver.py
@@ -41,7 +41,7 @@ class index:
         #return "Hello {0}".format(name)
 class frontend:
     def GET(self,person):
-        result = cloudserver.db.QueryPerson(person,0,2**10)
+        result = db.QueryPerson(person,0,2**10)
         data=json.dumps(result)
         
         return render.chart(data)
