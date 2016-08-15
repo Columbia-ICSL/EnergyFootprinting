c2fd462da3bc9f75d98b2b3f44a9e69902a238a7
peterjwei <peterjwei1993@gmail.com>
2016-08-15 19:47:09 -0400

    suggestion decisions added 2

--- suggestionDecisions.py
+++ suggestionDecisions.py
@@ -7,16 +7,16 @@ urls = (
 
 class DecisionVals:
 	def POST(self):
-        raw_data=web.data()
-        data = raw_data.split(',')
-        action = data[1]
-        if (action == "accept"):
-        	return "accepted"
-        elif (action == "decline"):
-        	return "declined"
-        return "what"
+		raw_data=web.data()
+		data = raw_data.split(',')
+		action = data[1]
+		if (action == "accept"):
+			return "accepted"
+		elif (action == "decline"):
+			return "declined"
+		return "what"
 
-    def GET(self):
-        return result
+	def GET(self):
+		return result
 
 Decisions = web.application(urls, locals());
