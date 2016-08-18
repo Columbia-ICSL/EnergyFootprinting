3f9deee3aee2c6e2d0c25fc01a53495c0d6edf17
peterjwei <peterjwei1993@gmail.com>
2016-08-17 23:03:00 -0400

    added sorted room occupancy data

--- suggestionsEngine.py
+++ suggestionsEngine.py
@@ -6,6 +6,8 @@ class suggestionsEngine:
 	moveLimit = 3
 	checkInterval = 60 #1 minute
 	moveUsers = []
+	sortedRoomList = ["nwc1000m_a1", "nwc1000m_a2", "nwc1000m_a3", "nwc1000m_a4", "nwc1000m_a5", "nwc1000m_a6", "nwc1000m_a7", "nwc1000m_a8", "nwc1003b", "nwc1003g", "nwc1008"]
+	sortedRoomOccupancy = [0] * len(sortedRoomOccupancy)
 	roomOccupancySnapshot = ""
 	changeScheduleUsers = []
 	turnOffApplianceUsers = []
@@ -17,12 +19,13 @@ class suggestionsEngine:
 		for roomID in list_of_rooms:
 			userList = list_of_rooms[roomID]
 			occupancy = len(userList)
-			self.roomOccupancySnapshot += str(roomID)
-			self.roomOccupancySnapshot += " "
-			self.roomOccupancySnapshot += str(occupancy)
-			self.roomOccupancySnapshot += ";"
+			index = self.sortedRoomList.index(roomID)
+			self.sortedRoomOccupancy[index] = occupancy
 			if (len(userList) < self.moveLimit):
 				users.extend(userList)
+		for i in range(len(sortedRoomOccupancy)):
+			self.roomOccupancySnapshot += str(self.sortedRoomOccupancy[i])
+			self.roomOccupancySnapshot += ";"
 		return users
 
 ############## TODO #################
