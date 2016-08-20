ac8fec8b0b7a1a6a911d24db0b2aefed91368f55
peterjwei <peterjwei1993@gmail.com>
2016-08-20 17:50:12 -0400

    screwed up reupload 3

--- locationTraining.py
+++ locationTraining.py
@@ -34,11 +34,11 @@ class train:
             x = f.readlines()
             cloudserver.trainingLabels = []
             for j in range(len(x)):
-                y = x[i].split('\t')
-                last = y[-1].split('\n')
-                y[-1] = last[0]
+                y = x[i]
+                last = y.split('\n')
+                y = last[0]
 
-                cloudserver.trainingLabels += [self.rooms.index(y[0])]               
+                cloudserver.trainingLabels += [self.rooms.index(y)]               
             return "successful reupload"
         if (locs[0] == "DES"):
             cloudserver.trainingData = []
