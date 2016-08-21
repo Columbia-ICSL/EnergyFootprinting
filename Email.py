c5a7a0ff5f2631515964c3882bd0c0c832aee1e3
Chen Xiaoqi <chenxq13@mails.tsinghua.edu.cn>
2016-08-21 18:34:18 -0400

    Change mail API

--- Email.py
+++ Email.py
@@ -3,9 +3,9 @@ import requests
 def SendEmail(title, body):
 	# TODO: verify sending API
     return requests.post(
-        "https://api.mailgun.net/v3/icsl.ee.columbia.edu/messages",
+        "https://api.mailgun.net/v3/sandbox5c480a7a94d14b3f9e4c7b1a52c3440b.mailgun.org/messages",
         auth=("api", "key-e9e526b55efd83ffe1c6a0b2200a5ea8"),
-        data={"from": "Mailgun Sandbox <postmaster@icsl.ee.columbia.edu>",
-              "to": "Xiaoqi <chenxiaoqino+mailgun@gmail.com>, Rishi <rc3022@columbia.edu>,",
+        data={"from": "Mailgun Sandbox <postmaster@sandbox5c480a7a94d14b3f9e4c7b1a52c3440b.mailgun.org>",
+              "to": ", Rishi <rc3022@columbia.edu>, Peter <pw2428@columbia.edu>",
               "subject": title,
               "text": body})
