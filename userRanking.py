61e26bf5e3eb3bd53b9cb92163ffb87cc42af691
peterjwei <peterjwei1993@gmail.com>
2016-08-27 17:58:17 -0400

    more ranking stuff 4

--- userRanking.py
+++ userRanking.py
@@ -8,7 +8,23 @@ urls = (
 class ranking:
 	def POST(self):
 		raw_data=web.data()
-		return cloudserver.db.getRankingData()
+		json_return = {
+			"user":raw_data,
+			"balance": 0,
+			"rankingData":[]
+		}
+		def make_entry(user, balance, Others={}):
+			Others.update({
+				"user":user,
+				"balance":balance
+				})
+			return Others
+		rankingData = cloudserver.db.getRankingData()
+		for rank in rankingData:
+			user = rank["user"]
+			balance = rank["balance"]
+			json_return["rankingData"].append(make_entry(user, balance))
+	return cloudserver.db._encode(json_return, False)
 
 	def GET(self):
 		return
