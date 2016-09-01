97af88b4e5cdb41bb9abb15048178189702fcc0c
peterjwei <peterjwei1993@gmail.com>
2016-09-01 15:12:17 -0400

    changed move suggestion 1

--- LocationBeacons.py
+++ LocationBeacons.py
@@ -47,15 +47,16 @@ class BeaconVals:
 
         json_return["location_id"]=self.labels[location]
         json_return["location"]=cloudserver.db.RoomIdToName(self.labels[location])
-        roomInfo = moveUsers[ID]
-        roomId=roomInfo["roomDest"]
-        roomName=cloudserver.db.RoomIdToName(roomId)
-        title="Move to "+roomName
-        body="Please consider sharing the room to lower everyone's energy footprint."
-        reward=4
-        json_return["suggestions"].append(
-            make_suggestion_item("move",title,body,reward,{"to":roomName,"to_id":roomId})
-        )
+        if (ID in moveUsers.keys()):
+            roomInfo = moveUsers[ID]
+            roomId=roomInfo["roomDest"]
+            roomName=cloudserver.db.RoomIdToName(roomId)
+            title="Move to "+roomName
+            body="Please consider sharing the room to lower everyone's energy footprint."
+            reward=4
+            json_return["suggestions"].append(
+                make_suggestion_item("move",title,body,reward,{"to":roomName,"to_id":roomId})
+            )
 
         for user in turnOffApplianceUsers:
             if (ID == user):
@@ -64,8 +65,10 @@ class BeaconVals:
                     applianceID = appliance["id"]
                     applianceName = appliance["name"]
                     powerUsage = appliance["value"]
+                    if (powerUsage < 10):
+                        continue
                     title="Shut off "+applianceName
-                    body=applianceName+" is consuming excess power ( watts), please see if you can switch off some appliance."
+                    body=applianceName+" is consuming excess power (" + str(powerUsage) + " watts), please see if you can switch off some appliance."
                     reward=1
                     json_return["suggestions"].append(
                         make_suggestion_item("turnoff",title, body, reward, {"appl":applianceName,"appl_id":applianceID, "power":powerUsage}))
