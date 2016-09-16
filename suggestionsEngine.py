d36ac0314c86733acc19ea5ade8a26a67126c70e
peterjwei <peterjwei1993@gmail.com>
2016-09-16 01:19:36 -0400

    added user schedules 10 added labs

--- suggestionsEngine.py
+++ suggestionsEngine.py
@@ -116,10 +116,10 @@ class suggestionsEngine:
 			for labName in xrange(len(startAvg)):
 				if (numUsers[labName] == 0):
 					numUsers[labName] = 1
-				startAvg[labName] = startAvg[labName]/numUsers[labName]
-				endAvg[labName] = endAvg[labName]/numUsers[labName]
+				startAvg[labName] = float(startAvg[labName]/numUsers[labName])
+				endAvg[labName] = float(endAvg[labName]/numUsers[labName])
 				labStrings = ["Burke", "Teherani", "Jiang", "Sajda", "Danino"]
-				print("avg: ------------------------ {0} {1}".format(str(datetime.datetime.fromtimestamp(startAvg)), str(datetime.datetime.fromtimestamp(endAvg))))
+				print("avg: ------------------------ {0} {1} lab {2}".format(str(datetime.datetime.fromtimestamp(startAvg)), str(datetime.datetime.fromtimestamp(endAvg)), str(labStrings[labName])))
 			for userRange in userDict:
 				labNum = userDict[userRange][2]
 				if ((userDict[userRange][0] > startAvg[labNum]) and (userDict[userRange][1] > endAvg[labNum])):
