5cc75022def0f6f302f20929625c1ba1d9e8e0a7
peterjwei <peterjwei1993@gmail.com>
2016-09-18 15:58:20 -0400

    added schedule analysis 2

--- suggestionsEngine.py
+++ suggestionsEngine.py
@@ -107,23 +107,23 @@ class suggestionsEngine:
 				for user_id in dict_users:
 					user_list.append(0)
 					return_bins = dict_users[user_id]
-            		for bin_start in sorted(return_bins.keys()):
-            			user_list.append(bin_start)
-            		break
-            	writer.writerow(user_list)
-            	user_list = []
-            	for user_id in dict_users:
-            		user_list = []
-            		userName = cloudserver.db.userIDLookup(user_id)
-            		user_list.append(userName)
-            		return_bins = dict_users[user_id]
-            		for bin_start in sorted(return_bins.keys()):
-            			BIN_ST = return_bins[bin_start]
-            			if (BIN_ST["value"] == None):
-            				user_list.append(0)
-            			else:
-            				user_list.append(BIN_ST["value"])
-            		writer.writerow(user_list)
+					for bin_start in sorted(return_bins.keys()):
+						user_list.append(bin_start)
+					break
+				writer.writerow(user_list)
+				user_list = []
+				for user_id in dict_users:
+					user_list = []
+					userName = cloudserver.db.userIDLookup(user_id)
+					user_list.append(userName)
+					return_bins = dict_users[user_id]
+					for bin_start in sorted(return_bins.keys()):
+						BIN_ST = return_bins[bin_start]
+						if (BIN_ST["value"] == None):
+							user_list.append(0)
+						else:
+							user_list.append(BIN_ST["value"])
+						writer.writerow(user_list)
 
 			for user_id in dict_users:
 				userStart = 0
