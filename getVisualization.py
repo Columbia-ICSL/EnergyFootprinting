7d79d13fb1eec373b05dfe6eb9dbd3ec8849024b
peterjwei <peterjwei1993@gmail.com>
2016-09-18 17:38:51 -0400

    added appliance visualization

--- getVisualization.py
+++ getVisualization.py
@@ -11,8 +11,8 @@ nowTime = datetime.datetime.now().replace(hour=4, minute=0)
 print("4AM today: " + str(nowTime))
 curtime = int(time.mktime(nowTime.timetuple()))-(n*86400)
 dict_users = db.BinUsersLocHistory(curtime-86400, curtime)
-numUsers = [0, 0, 0, 0, 0]
 userDict = {}
+roomDict = {}
 with open('changeScheduleTest.csv', 'w') as csvfile:
 	writer = csv.writer(csvfile, delimiter=' ',
 		quotechar='|', quoting=csv.QUOTE_MINIMAL)
@@ -37,3 +37,32 @@ with open('changeScheduleTest.csv', 'w') as csvfile:
 			else:
 				user_list.append(BIN_ST["value"])
 		writer.writerow(user_list)
+
+curtime = int(time.mktime(nowTime.timetuple()))-(n*86400)
+dict_appl = db.BinApplPowerHistory(curtime-86400, curtime)
+applDict = {}
+roomDict = {}
+with open('applianceVisualization.csv', 'w') as csvfile:
+	writer = csv.writer(csvfile, delimiter=' ',
+		quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	appl_list = []
+	appl_list.append(0)
+	return_bins = dict_appl[dict_appl.keys()[0]]
+	for bin_start in sorted(return_bins.keys()):
+		appl_list.append(bin_start)
+	writer.writerow(appl_list)
+	appl_list = []
+	for appl_id in dict_appl:
+		appl_list = []
+		applName = db.ApplIdToName(appl_id)
+		if applName == None:
+			applName = appl_id
+		appl_list.append(applName.strip())
+		return_bins = dict_appl[appl_id]
+		for bin_start in sorted(return_bins.keys()):
+			BIN_ST = return_bins[bin_start]
+			if (BIN_ST["value"] == None):
+				appl_list.append(0)
+			else:
+				appl_list.append(BIN_ST["value"])
+		writer.writerow(appl_list)
