3ce9861460c6a1677b9c90c3ea72f6b14106d18a
peterjwei <peterjwei1993@gmail.com>
2016-09-18 16:39:01 -0400

    removed visualization from server code

--- suggestionsEngine.py
+++ suggestionsEngine.py
@@ -100,31 +100,29 @@ class suggestionsEngine:
 			numUsers = [0, 0, 0, 0, 0]
 			userDict = {}
 
-			with open('changeSchedule.csv', 'w') as csvfile:
-				writer = csv.writer(csvfile, delimiter=' ',
-					quotechar='|', quoting=csv.QUOTE_MINIMAL)
-				user_list = []
-
-				user_list.append(0)
-
-				return_bins = dict_users[dict_users.keys()[0]]
-				for bin_start in sorted(return_bins.keys()):
-					user_list.append(bin_start)
-
-				writer.writerow(user_list)
-				user_list = []
-				for user_id in dict_users:
-					user_list = []
-					userName = cloudserver.db.userIDLookup(user_id)
-					user_list.append(userName)
-					return_bins = dict_users[user_id]
-					for bin_start in sorted(return_bins.keys()):
-						BIN_ST = return_bins[bin_start]
-						if (BIN_ST["value"] == None):
-							user_list.append(0)
-						else:
-							user_list.append(BIN_ST["value"])
-					writer.writerow(user_list)
+			#with open('changeSchedule.csv', 'w') as csvfile:
+			#	writer = csv.writer(csvfile, delimiter=' ',
+			#		quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			#	user_list = []
+#
+#				user_list.append(0)
+#				return_bins = dict_users[dict_users.keys()[0]]
+#				for bin_start in sorted(return_bins.keys()):
+#					user_list.append(bin_start)
+#				writer.writerow(user_list)
+#				user_list = []
+#				for user_id in dict_users:
+#					user_list = []
+#					userName = cloudserver.db.userIDLookup(user_id)
+#					user_list.append(userName)
+#					return_bins = dict_users[user_id]
+#					for bin_start in sorted(return_bins.keys()):
+#						BIN_ST = return_bins[bin_start]
+#						if (BIN_ST["value"] == None):
+#							user_list.append(0)
+#						else:
+#							user_list.append(BIN_ST["value"])
+#					writer.writerow(user_list)
 
 			for user_id in dict_users:
 				userStart = 0
