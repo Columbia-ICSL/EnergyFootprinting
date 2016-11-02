f3c12574ca20fd13c4bab5ea7c712326e20d37b5
peterjwei <peterjwei1993@gmail.com>
2016-11-02 15:29:30 -0400

    indentation 1

--- particleSensorCollection.py
+++ particleSensorCollection.py
@@ -9,7 +9,7 @@ class CollectParticles:
 	def POST(self, Id):
 		raw_data=web.data()
 		data=json.loads(raw_data)
-		cloudserver.db.particleSensorCollecting(Id, data['sensorNumber'], data['PM1L'], data['PM25L'], data['PM10L'], data['PM1A'], data['PM25A'], data['PM10A'], data['um03'], data['um05'], data['um1'], data['um25'], data['um50'], data['um10'])
+		cloudserver.db.particleSensorCollecting(Id, data['sensorNumber'])#, data['PM1L'], data['PM25L'], data['PM10L'], data['PM1A'], data['PM25A'], data['PM10A'], data['um03'], data['um05'], data['um1'], data['um25'], data['um50'], data['um10'])
 		print(str(data))
 		return "200 OK" 
 
