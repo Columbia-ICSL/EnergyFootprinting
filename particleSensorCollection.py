7a9c59a68cf5a8af18cde09881fe8e2b8213a4e4
peterjwei <peterjwei1993@gmail.com>
2016-11-02 15:33:20 -0400

    can't parse json

--- particleSensorCollection.py
+++ particleSensorCollection.py
@@ -8,6 +8,7 @@ urls = (
 class CollectParticles:
 	def POST(self, Id):
 		raw_data=web.data()
+		print(data)
 		data=json.loads(raw_data)
 		cloudserver.db.particleSensorCollecting(Id, data['sensorNumber'])#, data['PM1L'], data['PM25L'], data['PM10L'], data['PM1A'], data['PM25A'], data['PM10A'], data['um03'], data['um05'], data['um1'], data['um25'], data['um50'], data['um10'])
 		print(str(data))
