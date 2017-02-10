97c72b96d18db8e8b8e6c91c4b23e01b26eafcb1
peterjwei <peterjwei1993@gmail.com>
2017-02-09 19:46:07 -0500

    False = 1?

--- LocationBeacons.py
+++ LocationBeacons.py
@@ -74,10 +74,8 @@ class BeaconVals:
         synchronizeApplianceUsers = cloudserver.SE.synchronizeApplianceUsers
         phantomApplianceUsers = cloudserver.SE.phantomApplianceUsers
         balance_server = cloudserver.db.getUserBalance(ID)
-        print("balance_server")
-        print(balance_server)
         tempBalance_server = cloudserver.db.getUserTempBalance(ID)
-        if (balance_server == False):
+        if (balance_server is None):
             print(cloudserver.db.userIDLookup(ID))
             print("Balance error")
             balance_server = 0
