a6d7f91168e70c245fb49f0899d1acd789fe2259
peterjwei <peterjwei1993@gmail.com>
2017-02-28 14:07:17 -0500

    added control graphs, morning suggestion

--- appSupport.py
+++ appSupport.py
@@ -10,7 +10,14 @@ class appSupport:
 		if cloudserver.db.userIDLookup(data.id) == None :
 			return "Invalid userID"
 		else:
-			print "user found"
+			if cloudserver.db.getControl(data.id):
+				ret={
+					"value":0,
+					"HVAC":0,
+					"Light":0,
+					"Electrical":0
+				}
+				return ret
 			location = cloudserver.db.getUserLocation(data.id)
 			print location
 			ret = cloudserver.db.calculateEnergyFootprint(location)
