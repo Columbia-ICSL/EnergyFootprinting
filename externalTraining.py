1fe5924e72eea09e558caa8aaae1f6a8a7fa2ef5
peterjwei <peterjwei1993@gmail.com>
2017-03-28 00:28:28 -0400

    extra garbage

--- externalTraining.py
+++ externalTraining.py
@@ -10,15 +10,15 @@ class externalTraining:
 		locs = raw_data.split(',')
 		beaconData = locs[1:]
 		beaconString = ""
-		if (locs[0] == "NEW"):
-			outfile = "newData.txt"
-			outfile1 = "newDataLabels.txt"
-			f = open(outfile, "a+")
-			f1 = open(outfile1, "a+")
-			for i in range(len(beaconData)):
-				beaconString += beaconData[i]
-				if (i != (len(beaconData)-1)):
-					beaconString += "\t"
+		
+		outfile = "newData.txt"
+		outfile1 = "newDataLabels.txt"
+		f = open(outfile, "a+")
+		f1 = open(outfile1, "a+")
+		for i in range(len(beaconData)):
+			beaconString += beaconData[i]
+			if (i != (len(beaconData)-1)):
+				beaconString += "\t"
 		print(beaconString)
 		f.writeline(beaconString)
 		f1.writeline(locs[0])
