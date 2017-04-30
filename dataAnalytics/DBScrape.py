f92845a09eff3fb125c89ae2cf6e3d7103c87ab7
peterjwei <peterjwei1993@gmail.com>
2017-04-29 22:01:41 -0400

    class called too early

--- dataAnalytics/DBScrape.py
+++ dataAnalytics/DBScrape.py
@@ -1,7 +1,5 @@
 import pymongo
 
-db=DBScrape()
-
 class DBScrape():
 	def __init__(self):
 		self.dbc=pymongo.MongoClient()
@@ -9,3 +7,6 @@ class DBScrape():
 		self.registration_col1=self.dbc.db.registration_col1
 		print(list(self.registration_col1.find()))
 		return
+
+
+db=DBScrape()
