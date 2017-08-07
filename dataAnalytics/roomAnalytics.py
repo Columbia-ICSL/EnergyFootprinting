cb3e5e64470762097693a452d64890da916628c1
peterjwei <peterjwei1993@gmail.com>
2017-08-06 21:32:26 -0400

    added saved data error 3

--- dataAnalytics/roomAnalytics.py
+++ dataAnalytics/roomAnalytics.py
@@ -7,11 +7,19 @@ import os
 
 class roomAnalytics:
 	energyDictionary = {}
+	days = 30
+	def backspace(self):
+		print '\r',
+
 	def roomData(self):
 		databaseScrape = DBScrape.DBScrape()
 		t = (2017, 4, 1, 0, 0, 0, 0, 0, 0)
 		beginTime = calendar.timegm(datetime.datetime.utcfromtimestamp(time.mktime(t)).utctimetuple())
-		for i in range(0, 60):
+		for i in range(0, days):
+			s = str(round(float(i)/days*100.0)) + '%'
+			print s,
+			sys.stdout.flush()
+			self.backspace()
 			start = beginTime + i*24*60*60
 			end = beginTime + (i+1)*24*60*60
 			shots = databaseScrape.snapshots_col_appliances(start, end)
@@ -32,8 +40,8 @@ class roomAnalytics:
 					l = len(rooms)
 					for room in rooms:
 						if room not in self.energyDictionary:
-							self.energyDictionary[room] = [[0]*96 for index in range(60)]
-						if (day >= 60 or binNumber >= 96):
+							self.energyDictionary[room] = [[0]*96 for index in range(days)]
+						if (day >= days or binNumber >= 96):
 							print((binNumber, day))
 							continue
 						self.energyDictionary[room][day][binNumber] += energy/l
@@ -52,11 +60,11 @@ class roomAnalytics:
 				if (room != "nwc1003b_a"):
 					continue
 				arr = energyDictionary[room]
-				spamwriter.writerow(arr[30])
+				spamwriter.writerow(arr[days/2])
 				savedArray = [0]*96
 
 				for i in range(96):
-					for j in range(60):
+					for j in range(days):
 						savedArray[i] += arr[j][i]
 				for k in range(96):
 					savedArray[k] = savedArray[k]/96
