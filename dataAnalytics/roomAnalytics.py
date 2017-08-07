efcabec4d09bf032ff1b0f13986c5bcc91c6764a
peterjwei <peterjwei1993@gmail.com>
2017-08-06 21:24:32 -0400

    added saved data

--- dataAnalytics/roomAnalytics.py
+++ dataAnalytics/roomAnalytics.py
@@ -2,7 +2,8 @@ import DBScrape
 import calendar
 import datetime
 import time
-
+import csv
+import os
 
 class roomAnalytics:
 	energyDictionary = {}
@@ -18,16 +19,55 @@ class roomAnalytics:
 			for snapshot in shots:
 				timestamp = snapshot["timestamp"]
 				timestamp = calendar.timegm(timestamp.utctimetuple())
-				print(timestamp-start)
-				return
+				b = int(timestamp-start)/900
+				day = b/96
+				binNumber = b%96
 				data = snapshot["data"]
 				for appliance in data:
 					params = data[appliance]
 					energy = params["value"]
 					rooms = params["rooms"]
+					l = len(rooms)
 					for room in rooms:
 						if room not in self.energyDictionary:
 							self.energyDictionary[room] = [[0]*96 for index in range(60)]
+						if (day >= 60 or binNumber >= 96):
+							print((binNumber, day))
+							continue
+						self.energyDictionary[room][day][binNumber] += energy/l
+
+
+	def saveData(self):
+		try:
+			os.remove('savedData.csv')
+		except OSError:
+			pass
+		with open('savedData.csv', 'wb') as csvfile:
+			spamwriter = csv.writer(csvfile, delimiter=' ',
+									quotechar='|', quoting=csv.QUOTE_MINIMAL)
+			writeArray = []
+			for room in energyDictionary:
+				if (room != "nwc1003b_a"):
+					continue
+				arr = energyDictionary[room]
+				spamwriter.writerow(arr[30])
+				savedArray = [0]*96
+
+				for i in range(96):
+					for j in range(60):
+						savedArray[i] += arr[j][i]
+				for k in range(96):
+					savedArray[k] = savedArray[k]/96
+				spamwriter.writerow(savedArray)
+
+
+
+
+
+
+
+
 
 R = roomAnalytics()
 R.roomData()
+R.saveData()
