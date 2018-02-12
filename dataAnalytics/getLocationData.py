c738d7749b3375f8521499f8bb3be4d2277887f1
ICSL <icsl@xnode.fredjiang.com>
2018-02-12 10:09:23 -0500

    fixed location scraper

--- dataAnalytics/getLocationData.py
+++ dataAnalytics/getLocationData.py
@@ -11,14 +11,19 @@ class getLocationData:
 		self.locationDict = {}
 
 	def getSnapshots(self):
-		begin = datetime.datetime(2017, 2, 1)
+		begin = datetime.datetime(2017, 2, 12)
 		dayTime = begin
 		dayTomorrow = dayTime + datetime.timedelta(days=1)
+		totalLocations = 0
 		while dayTime < dayTime.now():
 			shots = self.databaseScrape.snapshots_col_users_location(dayTime, dayTomorrow)
+			print "Number of found users: " + str(len(self.locationDict))
+			print "Number of snapshots today: " + str(len(shots))
+			print "Number of cumulative locations: " + str(totalLocations)
 			for snapshot in shots:
 				timestamp = snapshot["timestamp"]
-				data = snapshots["data"]
+				data = snapshot["data"]
+				totalLocations += len(data)
 				for user in data:
 					person = user
 					location = data[user]["location"]
@@ -37,7 +42,7 @@ class getLocationData:
 			os.remove('locationDataFiles/locationData.csv')
 		except OSError:
 			pass
-		with open('locationDataFiles/locationData.csv') as csvfile:
+		with open('locationDataFiles/locationData.csv', 'wb') as csvfile:
 			spamwriter = csv.writer(csvfile, delimiter=' ',
 									quotechar='|', quoting=csv.QUOTE_MINIMAL)
 			L = len(self.locationDict)
