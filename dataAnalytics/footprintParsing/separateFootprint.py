cac399bfbc45f474380c5ea3cf40803e5a12b3da
Peter Wei <peterwei@dyn-128-59-247-6.dyn.columbia.edu>
2018-03-21 18:50:15 -0400

    added footprint parsing

---
+++ dataAnalytics/footprintParsing/separateFootprint.py
@@ -0,0 +1,89 @@
+import sys
+import datetime
+import os
+sys.path.append('/Users/peterwei/Desktop/python_webservice/dataAnalytics')
+#change to the path containing DBScrape.py.
+import DBScrape
+from spaceNames import S
+#contains the spaces
+print "This is the name of the script: ", sys.argv[0]
+print "Number of arguments: ", len(sys.argv)
+if len(sys.argv) != 7:
+	print "Supply arguments to this script. Example: python separateFootprint.py {beginYear} {beginMonth} {beginDay} {endYear} {endMonth} {endDay}"
+	return
+
+verbose = True
+def printC(text):
+	if verbose:
+		print text
+
+parameters = sys.argv[1:]
+
+class getFootprints:
+	def __init__(self):
+		self.databaseScrape = DBScrape.DBScrape()
+		self.timestamps = []
+		self.footprints = {}
+		self.spaces = S
+		print("Found " + str(len(self.spaces)) + " spaces")
+		for room in self.spaces:
+			self.footprints[room] = []
+
+	def getSnapshots(self, beginYear, beginMonth, beginDay, endYear, endMonth, endDay):
+		begin = datetime.datetime(beginYear, beginMonth, beginDay)
+		end = datetime.datetime(endYear, endMonth, endDay)
+		shots = self.databaseScrape.snapshots_col_appliances(begin, end)
+		printC("Found " + str(len(shots)) + " snapshots")
+		for shot in shots:
+			timestamp = shot["timestamp"]
+			pattern = '%Y-%m-%d %H:%M:%S'
+			epoch = int(time.mktime(time.strptime(date_time, pattern)))
+			self.timestamps.append(epoch)
+			for room in self.spaces:
+				self.spaces[room].append(0)
+
+			for applianceName in shot["data"]:
+				appliance = shot["data"][applianceName]
+				rooms = appliance["rooms"]
+				numRooms = len(rooms)
+				for room in rooms:
+					if room not in self.footprints:
+						print "room " + room + " not in space database"
+						continue
+					self.spaces[room][-1] = self.spaces[room][-1] + appliance["value"]/numRooms
+
+	def saveTimeSeries(self):
+		try:
+			os.remove('footprints.csv')
+		except OSError:
+			pass
+		with open('footprints.csv', 'wb') as csvfile:
+			footprintWriter = csv.writer(csvfile, delimiter=' ',
+					quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+			footprintWriter.writerow(["timestamps"] + self.timestamps)
+			for room in self.footprints:
+				footprintWriter.writerow([room] + self.footprints[room])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+GF = getFootprints()
+GF.getSnapshots(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5])
+GF.saveTimeSeries()
+
+
+
+
+
