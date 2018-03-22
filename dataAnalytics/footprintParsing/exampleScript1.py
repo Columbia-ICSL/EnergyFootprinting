aaf0301c9753c323e65daf5163961861de668793
Peter Wei <peterwei@dyn-128-59-247-6.dyn.columbia.edu>
2018-03-22 17:18:02 -0400

    pulled out script

---
+++ dataAnalytics/footprintParsing/exampleScript1.py
@@ -0,0 +1,22 @@
+import separateFootprint
+import plotGraphs
+
+from spaceNames import S
+#contains the spaces
+print "This is the name of the script: ", sys.argv[0]
+print "Number of arguments: ", len(sys.argv)
+if len(sys.argv) != 7:
+	print "Supply arguments to this script. Example: python separateFootprint.py {beginYear} {beginMonth} {beginDay} {endYear} {endMonth} {endDay}"
+	assert(len(sys.argv) == 7)
+verbose = True
+def printC(text):
+	if verbose:
+		print text
+
+parameters = sys.argv[1:]
+for i in range(len(parameters)):
+	parameters[i] = int(parameters[i])
+
+GF = getFootprints()
+GF.getSnapshots(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5])
+GF.saveTimeSeries()
---
+++ dataAnalytics/footprintParsing/plotGraphs.py
@@ -0,0 +1,5 @@
+
+
+class footprintPlots:
+	def __init__(self):
+		print "Hello World!"
