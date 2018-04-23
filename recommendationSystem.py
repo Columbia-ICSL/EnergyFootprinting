54ca9e7c2e0b95c38db4146b401c1db9bb970580
Peter Wei <peterwei@dyn-128-59-247-6.dyn.columbia.edu>
2018-04-23 10:11:13 -0400

    added users to recommendation dictionary

--- recommendationSystem.py
+++ recommendationSystem.py
@@ -12,13 +12,30 @@ class recommenderSystem:
 	def setup(self):
 		self.checkInterval = 30
 		self.users = None
+		self.userRecommendations = {}
 		self.getUsers()
+	
 
 	def getUsers(self):
 		#create a list of users from the database
 		self.users = cloudserver.db.dumpUsers()
 		print "Loaded " + str(len(self.users)) + " users"
+		for user in self.users:
+			self.userRecommendations[user["userID"]] = user["name"]
+		print "Loaded user recommendations dictionary"
+		print self.userRecommendations
 
+	def loadBuildingParams(self):
+		return
+
+	def getUserLocations(self):
+		return
+
+	def returnRecs(self, user):
+		return
+
+	def bestRecommendations(self):
+		return
 
 
 
