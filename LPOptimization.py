e2b187d11a77911e3dd6dabaed3c5d0389d2ccfa
ICSL <icsl@xnode.fredjiang.com>
2018-04-25 12:06:58 -0400

    fixed LP Optimization

--- LPOptimization.py
+++ LPOptimization.py
@@ -1,10 +1,11 @@
 import numpy as np
 import cvxopt
+from cvxopt import glpk
 
 c = cvxopt.matrix([10, 11, 19, 12], tc='d')
-G = cvxopt.matrix([[3], [3], [4], [3]], tc='d')
-h = cvxopt.matrix([7], tc='d')
-(status, x) = cvxopt.glpk.ilp(c,G.T,h,I=set([0,1,2,3]), B=set([0,1,2,3]))
+G = cvxopt.matrix([[-3], [-3], [-4], [-3]], tc='d')
+h = cvxopt.matrix([-8], tc='d')
+(status, x) = cvxopt.glpk.ilp(c,G,h,I=set([0,1,2,3]), B=set([0,1,2,3]))
 print(status)
 print((x[0],x[1],x[2],x[3]))
 
