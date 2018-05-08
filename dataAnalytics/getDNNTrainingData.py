d939fab9fafb8a377c72601211ccff64503141fb
Peter Wei <peterwei@dyn-128-59-247-6.dyn.columbia.edu>
2018-05-08 16:28:21 -0400

    import sys

--- dataAnalytics/getDNNTrainingData.py
+++ dataAnalytics/getDNNTrainingData.py
@@ -16,9 +16,7 @@ parameters = sys.argv[1:]
 for i in range(len(parameters)):
 	parameters[i] = int(parameters[i])
 
-GF = getTrainingData(S, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5], True)
-GF.getSnapshots()
-GF.recommendations()
+
 
 class getTrainingData:
 	def __init__(self, startYear, startMonth, startDay, endYear, endMonth, endDay, verbose):
@@ -212,6 +210,9 @@ class getTrainingData:
 		with open("y.csv", "a") as labelFile:
 			labelFile.write(recs)
 
+GF = getTrainingData(S, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5], True)
+GF.getSnapshots()
+GF.recommendations()
 
 
 
