393f11371a33e2c2947ea359de30dc5978fc0eab
Peter Wei <peterwei@dyn-128-59-247-6.dyn.columbia.edu>
2018-05-11 12:13:05 -0400

    added changed files

--- dataAnalytics/personal.py
+++ dataAnalytics/personal.py
@@ -3,3 +3,5 @@ P = ["nwc1007_plug1", "nwc1007_plug2", "nwc1008_plug1", "nwc1008_light", "nwc100
 	"nwc1000m_a1_plug3", "nwc1000m_a1_plug1", "nwc1000m_a1_plug2", "nwc1000m_a2_plug1", "nwc1000m_a2_plug2", "nwc1000m_a3_plug1", "nwc1000m_a3_plug2",
 	"nwc1000m_a4_plug1", "nwc1000m_a4_plug2", "nwc1000m_a5_plug1", "nwc1000m_a5_plug2", "nwc1000m_a6_plug1", "nwc1000m_a6_plug2",
 	"nwc1000m_a7_plug1", "nwc1000m_a7_plug2", "nwc1000m_a8_plug1", "nwc1000m_a8_plug2"]
+
+PO = [None, None, "Professor Jiang", "Professor Jiang", "Peter", "Stephen", "Kevin"]
